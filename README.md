4dc2332534b9c7b983f2e7a3ec8057f2bf7462f4
taylor124 <74930599+taylor124@users.noreply.github.com>
2021-01-25 17:32:51 -0500

    Update: weather dashboard
    weather dashboard is now completed

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Weather-Dashboard
-Create a weather dashboard
+
+This Weather Dashboard displays the Weather for countries, towns and cities using input fields that javascript has code assigned to.
+Last location searched will re-appear on the page once reloaded(exceptions may apply).
+
+# HTML
+
+* List Elements along side Forms where it applies the information received from Onecall in javascript.
+* Also receives style from bootstrap.
+
+# JavaScript
+
+* hosts a localStorage function
+* function creates buttons when locations are searched, saves on reload.
+* displays the last thing searched(last object in the array) on page reload.
+
+# CSS
+
+* most of the styling is done by yours truly, other is from bootstrap.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,94 @@
-var searchBtn = document.querySelector('#searchBtn')
+var searchBtn = document.querySelector('#searchBtn');
 var inputField = document.querySelector('#cityInput');
+var formEl = document.querySelector('#localSearchesForm');
+var localSearchEl = document.querySelector('.list-group-item');
+var weatherBtn = document.querySelector('.weatherBtn');
+var previousSearches = [];
 
-searchBtn.addEventListener('click', function(e){
+//Creates buttons after submitting weather criteria
+
+var DisplaySearch = function () {
+    previousSearches = JSON.parse(localStorage.getItem('weatherCriteria'));
+
+
+    if (previousSearches !== null) {
+
+        weatherCall(previousSearches[previousSearches.length - 1]);
+        for (var i = 0; i < previousSearches.length; i++) {
+            var liResults = document.createElement('li');
+            liResults.classList.add('list-group-item');
+            liResults.textContent = "";
+            formEl.appendChild(liResults);
+            var weatherBtn = document.createElement('button');
+            weatherBtn.textContent = previousSearches[i];
+            weatherBtn.classList.add('btn', 'btn-light');
+            liResults.appendChild(weatherBtn);
+
+            weatherBtn.addEventListener('click', function (e) {
+                e.preventDefault();
+                console.log(e);
+                weatherCall(this.innerText);
+            });
+        };
+    }
+    else {
+        previousSearches = [];
+    };
+};
+
+document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault()
-    var userInput = inputField.value
+    var userInput = inputField.value;
     console.log(userInput);
     weatherCall(userInput);
+    console.log(e);
+
+    var cityLocal = document.createElement('button');
+    cityLocal.textContent = userInput;
+    cityLocal.classList.add('list-group-item');
+
+
+    var formEl = document.querySelector('#localSearchesForm');
+    formEl.appendChild(cityLocal);
+
+    console.log(previousSearches);
+    previousSearches.push(userInput);
+    SavedContent();
+
 });
 
-inputField.addEventListener('submit', function(e){
-    e.preventDefault()
-    var userInput = inputField.value
-    console.log(userInput);
-    weatherCall(userInput);
-});
+// Gathers Info from oneCall
 
-var weatherCall = function(cityName){
+var weatherCall = function (cityName) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=39428f6d41d40bd58ecee861a07b8de4"
-    $.ajax({url:queryURL, method:'GET'}).then(function(weatherRes){
-        //console.log(weatherRes, weatherRes.coord.lat, weatherRes.coord.lon)
+    $.ajax({ url: queryURL, method: 'GET' }).then(function (weatherRes) {
+        console.log(weatherRes, weatherRes.coord.lat, weatherRes.coord.lon)
         oneCall(weatherRes.coord.lat, weatherRes.coord.lon);
     });
 };
 
-var oneCall = function(Lat, Lon){
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + Lat + "&lon=" + Lon +  "&exclude=minutely,hourly&units=metric&appid=39428f6d41d40bd58ecee861a07b8de4"
-    $.ajax({url:queryURL, method:'GET'}).then(function(weatherResCord){
+// Applies Info into HTML from here
+
+var oneCall = function (Lat, Lon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + Lat + "&lon=" + Lon + "&exclude=minutely,hourly&units=metric&appid=39428f6d41d40bd58ecee861a07b8de4"
+    $.ajax({ url: queryURL, method: 'GET' }).then(function (weatherResCord) {
         console.log(weatherResCord);
         //Update HTML with current weather data with weatherResCord and forecast
+        var divElDisplay1 = document.querySelector('.Description');
+        var divElDisplay2 = document.querySelector('.Humidity');
+        var divElDisplay3 = document.querySelector('.Tempature');
+        var divElDisplay4 = document.querySelector('.Feels-Like');
+        divElDisplay1.textContent = weatherResCord.current.weather[0].description;
+        divElDisplay2.textContent = weatherResCord.current.humidity + "%";
+        divElDisplay3.textContent = weatherResCord.current.temp + "C°";
+        divElDisplay4.textContent = weatherResCord.current.feels_like + "C°";
     });
 };
 
+// localStorage Function
+
+var SavedContent = function () {
+    localStorage.setItem('weatherCriteria', JSON.stringify(previousSearches));
+};
+
+DisplaySearch();
